f1e7551aba68691a9311d4335df7cec3960efe1c
Vuthikorn DR.eatha <vuthikorpannakh@gmail.com>
2026-09-08 21:23:18 +0700

    start

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://kkngrqvmkdpsfphvajjm.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrbmdycXZta2Rwc2ZwaHZhamptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4Njc5MDYsImV4cCI6MjEwNDQ0MzkwNn0.IspQEkGAuFcslv7KfxuWc2es5FPkkZ8uX3AkS8TGero'
+};
